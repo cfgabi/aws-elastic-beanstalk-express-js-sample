eb67f5fd64f40118f7b89a288296f0ed161cc2e4
cfgabi <cfgabi@hotmail.com>
2021-11-03 17:23:41 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('E aí, galera?'));
+app.get('/', (req, res) => res.send('O que é que há, velhinho?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
